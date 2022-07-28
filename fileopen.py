a6e8a06f147346c0c9113ea1059acba635236ee2
Ellison Gregg <agreg016@gmail.com>
2022-07-28 17:26:00 -0500

    Method Change, added comments
    Added in comments. while loop to for loop change. Still ain't workin'. Can't seem to find method for removing lines in python.

--- fileopen.py
+++ fileopen.py
@@ -4,23 +4,60 @@
 # find index for end of block
 # delete lines between indexes
 
+
+from os import remove
+
 def delete28fps():
-    lines = ctFile.readlines()
+    if ctFile.writable() != True:
+        print("Cannot open file")
+        return
+
+    else:
+
+        lines = ctFile.readlines()
+        print("Reading lines...")
 
 
     # lets itirate through these "lines"
-    for i, lines in enumerate(lines):
+    # idk why "line" is included in the 'for' loop statement,
+    #       but here we are. It does not seem to itirate
+    #       properly w/o it.
+    # print statments in there just to see where the program is at from
+    #   terminal.
+
+    for i, line in enumerate(lines):
+        
+        print(lines[i])
+
         if "28.6fps" in lines[i]:
-            while lines[i] != "End Export":
-                list.remove(lines)   
+            print(lines[i])
+
+            while lines[i] != "End Export\n":
+                print(lines[i])
+                # where file.removeLine() !?
+                # Can't seem to find a fn that will "truncate"
+                #   a single line from a file
+                i += 1
+            return    
+        
 
         else:
             print("nothing to strip")
-            break
-
+    
+    
     ctFile.close()
+    print("File Closed.")
 
-
+# Open up file via user CLI input with w/r privilages
 ctFile = open(input("Enter File name: "), 'r+')
 
-delete28fps
+# Really not needed, but add statment noting if file was found
+#  and actually writeable
+print("File Open and writeable: ", ctFile.writable())
+
+# Call the fn that deletes code from script file for 28.6fps exporting
+delete28fps()
+
+# Close file after calling fn
+ctFile.close()
+print("File Closed.")
